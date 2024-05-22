b9fb52edae4d98a103de1fe7ccdba1357afb3790
Rogerio Felix <rogerio.felix.ext@espm.br>
2024-05-22 15:05:19 -0300

    fix: className prop to CarouselWrapper component

--- src/next-elastic-carousel/components/styled/CarouselWrapper.js
+++ src/next-elastic-carousel/components/styled/CarouselWrapper.js
@@ -8,9 +8,9 @@ const CarouselWrapperDiv = styled.div`
   direction: ${({ isRTL }) => (isRTL ? 'rtl' : 'ltr')};
 `;
 
-export default function CarouselWrapper({ isRTL, children }) {
+export default function CarouselWrapper({ isRTL, children, className }) {
 
   return <StyleSheetManager shouldForwardProp={(prop) => prop !== 'isRTL'}>
-    <CarouselWrapperDiv isRTL={isRTL}>{children}</CarouselWrapperDiv>
+    <CarouselWrapperDiv isRTL={isRTL} className={className}>{children}</CarouselWrapperDiv>
   </StyleSheetManager>
 }
