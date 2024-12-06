569ee752cc18782842a65c588c9c16cbe2f21dc7
Rogerio Felix <ramuhs256@hotmail.com>
2024-12-06 10:49:19 -0300

    refactor: remove unused class

--- src/next-elastic-carousel/components/styled/ItemWrapper.js
+++ src/next-elastic-carousel/components/styled/ItemWrapper.js
@@ -1,43 +1,48 @@
-import styled, { StyleSheetManager } from "styled-components";
-import PropTypes from "prop-types";
-import { cssPrefix } from "../../utils/helpers";
-import consts from "../../constants";
+import styled, { StyleSheetManager } from 'styled-components'
+import PropTypes from 'prop-types'
+import { cssPrefix } from '../../utils/helpers'
+import consts from '../../constants'
 
 const ItemWrapper = styled.div.attrs(({ style }) => ({
   style,
-  className: cssPrefix("item-wrapper xxx")
+  className: cssPrefix('item-wrapper'),
 }))`
   box-sizing: border-box;
   display: flex;
   overflow: hidden;
   user-select: none;
   justify-content: ${({ itemposition }) => itemposition};
-`;
+`
 
 ItemWrapper.defaultProps = {
   style: {},
-  itemposition: consts.CENTER
-};
+  itemposition: consts.CENTER,
+}
 
 ItemWrapper.propTypes = {
   children: PropTypes.element.isRequired,
   style: PropTypes.object,
-  itemPosition: PropTypes.oneOf([consts.START, consts.CENTER, consts.END])
-};
+  itemPosition: PropTypes.oneOf([consts.START, consts.CENTER, consts.END]),
+}
 
-
-
-export default function ItemWrapperContainer({ itemPosition, children, style, idx, onItemClick }) {
-
-  return <StyleSheetManager shouldForwardProp={(prop) => prop !== 'itemPosition'}>
-    <ItemWrapper
-      id={idx}
-      itemPosition={itemPosition}
-      style={style}
-      key={idx}
-      onClick={onItemClick}
-    >
-      {children}
-    </ItemWrapper>
-  </StyleSheetManager>
+export default function ItemWrapperContainer({
+  itemPosition,
+  children,
+  style,
+  idx,
+  onItemClick,
+}) {
+  return (
+    <StyleSheetManager shouldForwardProp={(prop) => prop !== 'itemPosition'}>
+      <ItemWrapper
+        id={idx}
+        itemPosition={itemPosition}
+        style={style}
+        key={idx}
+        onClick={onItemClick}
+      >
+        {children}
+      </ItemWrapper>
+    </StyleSheetManager>
+  )
 }
